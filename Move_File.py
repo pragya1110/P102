e1d0158a8f9acc5c45b371fe368fe8d247c05bc0
pragya1110 <74659398+pragya1110@users.noreply.github.com>
2023-06-11 14:07:55 +0530

    Add files via upload

---
+++ Move_File.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+frmdir="C:/Users/pragy/Downloads"
+todir="C:/Users/pragy/OneDrive/Desktop/projects/Document_files"
+
+lof=os.listdir(frmdir)
+#print(lof)
+
+for fn in lof:
+    root,ext=os.path.splitext(fn)
+    if ext=='':
+        continue
+    else:
+        if ext in ['.txt','.doc','.docx','.pdf']:
+            path1= frmdir+'/'+fn
+            path2= todir+'/'+"Document_files"
+            path3=path2+'/'+fn
+            if os.path.exists(path2):
+                print("moving "+fn+"...")
+                shutil.move(path1,path3)
+            else:
+                os.makedirs(path2)
+                print("moving"+fn+"...")
+                shutil.move(path1,path3)
+               
